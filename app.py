16b00ae4452371de2c73724f6c8bf628bdc566ab
Imon Roy <royimonroy@gmail.com>
2021-08-20 16:09:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,6 @@ def home():
         return render_template('index.html',visib=visib,res=res)
     elif request.method=='POST':
         data = request.form.to_dict()
-        return jsonify(data)
         if 'stop_bomb' in data:
             if 'number' not in data:
                 return url_for('home',res='NUMBER NOT PROVIDED')
@@ -35,17 +34,17 @@ def home():
                 blocked.insert_one({'number':data['number']})
                 return url_for('home',res='NUMBER WILL NOT BE BOMBED FROM NOW ON')
         else:
-            if 'number' not in data:
+            if data['number']=="":
                 return url_for('home',res='NUMBER NOT PROVIDED')
-            if 'freq' not in data:
+            if data['freq']=="":
                 return url_for('home',res='PLEASE SELECT THE NUMBER OF MESSAGES TO SEND')
-            if 'interval' not in data:
+            if 'interval'=="":
                 data['interval']=5
             else:
                 block = blocked.find_one({"number":data["number"]})
             if block is None:
                 from main import bomber
-                Thread(target=bomber(data['number'],data['freq'],data['interval']),args=(data['number'],int(data['freq']),int(data['interval'],)))
+                Thread(target=bomber(),args=(data['number'],int(data['freq']),int(data['interval'],)))
                 return url_for('home',res='BOMBING STARTED SUCCESSFULLY')
             else:
                 return url_for('home',res='NUMBER CANNOT BE BOMBED')   
