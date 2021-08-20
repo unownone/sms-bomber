35eb099a26ff3095cbfa0dbe01f5e3ebc4515550
Imon Roy <royimonroy@gmail.com>
2021-08-20 16:44:41 +0530

    Update drivers.py

--- drivers.py
+++ drivers.py
@@ -8,7 +8,7 @@ chrome_options.binary_location = app.config["GOOGLE_CHROME_BIN"]
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=app.config["CHROMEDRIVER_PATH"], chrome_options=chrome_options)
+browser = webdriver.Chrome(executable_path=app.config["CHROMEDRIVER_PATH"], chrome_options=chrome_options)
 
 def flipkart(mob):
     browser.get('https://www.flipkart.com/account/login?ret=/')
