d9db814f6e551e0f02a9dd6963afd3c58fb0972b
Imon Roy <royimonroy@gmail.com>
2021-08-20 17:58:23 +0530

    adding a proper footer?

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from config import Config
 from flask_pymongo import PyMongo
 from threading import Thread
 
-
+visited=0
+unique = 0
 app = Flask(__name__)
 
 app.config.from_object(Config)
@@ -11,9 +12,10 @@ app.config.from_object(Config)
 mongo = PyMongo(app)
 
 blocked = mongo.db.restricted
+logging = mongo.db.logging
 
 @app.route('/',methods=['GET','POST'])
-def home():
+def home(self):
     if request.method=='GET':
         data=request.args.to_dict()
         if 'res' not in data:
@@ -24,20 +26,20 @@ def home():
         else:
             visib=''
             res=data['res']
-        return render_template('index.html',visib=visib,res=res)
+        return render_template('index.html',visib=visib,res=res,visits=self.visited,uniq=self.unique)
     elif request.method=='POST':
         data = request.form.to_dict()
         if 'stop_bomb' in data:
             if 'number' not in data:
-                return redirect(url_for('home',res='NUMBER NOT PROVIDED'))
+                return redirect(url_for('home',res='NUMBER NOT PROVIDED',visits=self.visited,uniq=self.unique))
             else:
                 blocked.insert_one({'number':data['number']})
-                return redirect(url_for('home',res='NUMBER WILL NOT BE BOMBED FROM NOW ON'))
+                return redirect(url_for('home',res='NUMBER WILL NOT BE BOMBED FROM NOW ON',visits=self.visited,uniq=self.unique))
         else:
             if data['number']=="":
-                return redirect(url_for('home',res='NUMBER NOT PROVIDED'))
+                return redirect(url_for('home',res='NUMBER NOT PROVIDED',visits=self.visited,uniq=self.unique))
             if data['freq']=="":
-                return redirect(url_for('home',res='PLEASE SELECT THE NUMBER OF MESSAGES TO SEND'))
+                return redirect(url_for('home',res='PLEASE SELECT THE NUMBER OF MESSAGES TO SEND',visits=self.visited,uniq=self.unique))
             if 'interval'=="":
                 data['interval']=5
             else:
@@ -46,10 +48,18 @@ def home():
                 from main import bomber
                 thread = Thread(target=bomber,args=(data['number'],int(data['freq']),int(data['interval'])))
                 thread.start()
-                return redirect(url_for('home',res='BOMBING STARTED SUCCESSFULLY'))
+                datea = request.headers['X-Forwarded-For']
+                logg = logging.find_one_and_update({"info":"total views"},{{"$inc":{"total_views":1}},{"$push": {"ips":datea}}})
+                datea = logging.find_one({"info":"total views"})
+                self.visited = datea["total_views"]
+                self.unique = len(datea["ips"])
+                return redirect(url_for('home',res='BOMBING STARTED SUCCESSFULLY',visits=self.visited,uniq=self.unique))
             else:
-                return redirect(url_for('home',res='NUMBER CANNOT BE BOMBED'))
+                return redirect(url_for('home',res='NUMBER CANNOT BE BOMBED',visits=self.visited,uniq=self.unique))
             
 
 if __name__=='__main__':
+    visits = logging.find_one({"info":"total views"})
+    visited = visits["total_views"]
+    unique = len(visits["ips"])
     app.run(debug=False)
