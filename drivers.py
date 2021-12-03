3f4465efc3de9261b77ed69372f74f334f9766b8
unownone <38348296+unownone@users.noreply.github.com>
2021-12-03 20:23:21 +0530

    possible fix?

--- drivers.py
+++ drivers.py
@@ -1,15 +1,15 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.chrome.options import Options
-from flask import current_app as app
+from config import Config
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = app.config["GOOGLE_CHROME_BIN"]
+chrome_options.binary_location = Config.GOOGLE_CHROME_BIN
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument("--no-sandbox")
-browser = webdriver.Chrome(executable_path=app.config["CHROMEDRIVER_PATH"], chrome_options=chrome_options)
+browser = webdriver.Chrome(executable_path=Config.CHROMEDRIVER_PATH, chrome_options=chrome_options)
 
 def flipkart(mob):
     browser.get('https://www.flipkart.com/account/login?ret=/')
